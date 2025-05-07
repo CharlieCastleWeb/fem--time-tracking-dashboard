7e682c5f586185f8e040313cc58de274f2129b38
Carlos Castillo <castillomartinezcarlos@gmail.com>
2025-05-07 18:11:57 +0200

    feat: user profile card styles

---
+++ src/components/MainLayout.ts
@@ -0,0 +1,8 @@
+import { renderUserProfileCard } from "./UserProfileCard";
+
+export function renderMainLayout(): HTMLElement {
+  const container = document.createElement("section");
+  container.classList.add("main-layout");
+  container.appendChild(renderUserProfileCard());
+  return container;
+}
---
+++ src/components/UserProfileCard.ts
@@ -0,0 +1,21 @@
+export function renderUserProfileCard(): HTMLElement {
+  const container = document.createElement("div");
+  container.innerHTML = `
+    <div class="user-profile-card">
+      <div class="user-profile-card__header">
+        <img src="/images/image-jeremy.png" alt="Jeremy Robson" class="user-profile-card__avatar"/>
+        <div class="user-profile-card__info">
+          <p class="user-profile-card__label">Report for</p>
+          <p class="user-profile-card__name">Jeremy Robson</p>
+        </div>
+      </div>
+      <div class="user-profile-card__timeframe-selector">
+        <button class="user-profile-card__button">Daily</button>
+        <button class="user-profile-card__button">Weekly</button>
+        <button class="user-profile-card__button">Monthly</button>
+      </div>
+    </div>
+  `;
+
+  return container;
+}
