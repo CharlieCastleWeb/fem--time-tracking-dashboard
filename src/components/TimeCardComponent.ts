6715914901e8fbf1ebfd8dc6af747ec1dd6cc230
Carlos Castillo <castillomartinezcarlos@gmail.com>
2025-05-12 22:44:36 +0200

    feat: change card background color and icon

--- src/components/TimeCardComponent.ts
+++ src/components/TimeCardComponent.ts
@@ -1,9 +1,17 @@
 import type { Activity } from "../interfaces/activity.interface";
 import { renderTimes } from "./Times";
 
+const getCardClass = (title: string): string => {
+  return `activity-card--${title.toLowerCase().replace(/\s+/g, "-")}`;
+};
+
 export function renderTimeCard(activity: Activity): HTMLElement {
   const container = document.createElement("div");
   container.classList.add("activity-card");
+
+  const modifierClass = getCardClass(activity.title);
+  container.classList.add(modifierClass);
+
   container.innerHTML = `
     <div class="activity-card__body">
       <div class="activity-card__header">
