06fd30791600146d19f13b159d117226e1807384
Carlos Castillo <castillomartinezcarlos@gmail.com>
2025-05-14 23:55:21 +0200

    feat: code refactor

--- src/components/UserProfileCard.ts
+++ src/components/UserProfileCard.ts
@@ -10,7 +10,7 @@ export function renderUserProfileCard(): HTMLElement {
   container.classList.add("user-profile-card");
   container.innerHTML = `
     <div class="user-profile-card__header">
-      <img src="/images/image-jeremy.png" alt="Jeremy Robson" class="user-profile-card__avatar"/>
+      <img src="./images/image-jeremy.png" alt="Jeremy Robson" class="user-profile-card__avatar"/>
       <div class="user-profile-card__info">
         <p class="user-profile-card__label">Report for</p>
         <p class="user-profile-card__name">Jeremy Robson</p>
